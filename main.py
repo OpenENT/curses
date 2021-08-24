53c6dd8d247c585d64d4a0a0ce16e598fcc9ccb7
Mast03 <antoniobased@icloud.com>
2021-08-25 00:15:12 +0200

    Start transition from raw curses code to intents

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from ui import PlayingStatusIntent, TitleBarIntent, ConsoleIntent, MainIntent
 from clients import PlayerD, Backend
+from console import Console
 from curses import wrapper
-from ui import MainIntent
 
 import curses
 import time
@@ -9,82 +10,57 @@ import time
 c = PlayerD("http://127.0.0.1:5000")
 b = Backend("http://127.0.0.1:5001")
 
+playingstatus = PlayingStatusIntent(c)
+titlebar = TitleBarIntent()
+console = ConsoleIntent(Console(c, b))
+
 console_override = False
 intents = [MainIntent()]
 index = 0
-text = ""
 
-
-def draw_titlebar(stdscr):
-    # Shitfest alert
-    h, w = stdscr.getmaxyx()
-    str = "OpenPlayer"
-    strlen = len(str)
-    padding = " " * int((w - strlen) / 2)
-    str = padding + str + padding
-    #stdscr.addstr(0, 0, " " * (w - 1)) # Clear line
-    stdscr.addstr(0, 0, str, curses.color_pair(1))
-
-def draw_playingbar(stdscr):
-    s = c.get_status()
-    h, w = stdscr.getmaxyx()
-    stdscr.addstr(h-1, 0, " "*(w-1), curses.color_pair(1))
-    if s['playing']:
-        current = int(s['position'])
-        duration = s['duration']
-        stdscr.addstr(h-1, 0, f"{current} / {duration} - {s['name']}", curses.color_pair(1))
-    else:
-        stdscr.addstr(h-1, 0, "Not playing", curses.color_pair(1))
-
-def draw_console(stdscr):
-    if not console_override:
-        return
-    h, w = stdscr.getmaxyx()
-    if len(text) == 0:
-        render_text = "..."
-    else:
-        render_text = text
-    stdscr.addstr(h-1, 0, " " * (w - 1), curses.color_pair(1)) # Clear line
-    stdscr.addstr(h-1, 0, ">" + render_text, curses.color_pair(1))
+old_w = 0
+old_h = 0
 
 def execute():
     pass
 
 def handle_input(stdscr):
 
-    global console_override
-    global text
-    
-    x = stdscr.getch()
+    global console_override    
+    char = stdscr.getch()
 
-    if x == -1:
+    if char == -1:
         return
-    elif x == 62:
+    elif char == 62:
         console_override = not console_override
     elif console_override:
-        if x == 127:
-            if len(text) == 0:
-                return
-            text = text[:-1]
-        elif x == 263:
-            text = ""
-        elif x == 10:
-            execute()
+        ret, intent = console.input(char)
+        if ret:
             console_override = False
-        else:
-            text += chr(x)
+        if intent is not None:
+            intents.append(intent)
     else:
-        intents[-1].input(x)  # TODO: handle intent change
+        intents[-1].input(char)  # TODO: handle intent change
 
 
 def render(stdscr):
-    draw_titlebar(stdscr)
-    draw_playingbar(stdscr)
-    draw_console(stdscr)
+
+    global old_h, old_w
     h, w = stdscr.getmaxyx()
+
+    if old_h != h or old_w != w:
+        stdscr.clear()
+        old_h = h
+        old_w = w
+    
+    titlebar.render(stdscr, 0, 0, w, 1)
+    playingstatus.render(stdscr, 0, h-1, w, 1)
+    if console_override:
+        console.render(stdscr, 0, h-1, w, 1)
     intents[-1].render(stdscr, 0, 1, w, h-2)
     handle_input(stdscr)
     stdscr.refresh()
+
     return True
 
 def main(stdscr):
