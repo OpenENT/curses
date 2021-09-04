2ce3be116a4151de382a6e41e2dc22405bd01b2f
Mast03 <antoniobased@icloud.com>
2021-09-04 08:55:29 +0000

    Settings load/save support

--- settings.py
+++ settings.py
@@ -6,7 +6,11 @@ def ProviderSettings(provider, prefer_download=True):
 class Settings:
 
     def __init__(self, path):
+        self.path = path
         self.providers = dict()
+        with open(self.path) as f:
+            self.__dict__.update(json.load(f))
 
     def save(self):
-        pass
+        with open(self.path, 'w') as f:
+            json.dump(self.__dict__, f)
