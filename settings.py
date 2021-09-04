dca43fc10c0c83de13825cc1ac5e5c31ee54f997
Mast03 <antoniobased@icloud.com>
2021-09-04 09:30:07 +0000

    playerd/backend address

--- settings.py
+++ settings.py
@@ -8,6 +8,8 @@ class Settings:
     def __init__(self, path):
         self.path = path
         self.providers = dict()
+        self.playerd = 'http://127.0.0.1:5000'
+        self.backend = 'http://127.0.0.1:5001'
         with open(self.path) as f:
             self.__dict__.update(json.load(f))
 
